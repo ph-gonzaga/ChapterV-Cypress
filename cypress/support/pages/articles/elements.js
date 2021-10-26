715fcbd1ace43d695afee4b689e2baab3fe6d572
Pedro Henrique Gonzaga <henrique.gonzaga@live.com>
2021-10-26 15:46:35 -0300

    teste erro

--- cypress/support/pages/articles/elements.js
+++ cypress/support/pages/articles/elements.js
@@ -2,7 +2,7 @@ export const EL = {
 
   inputTitle: '[ng-model$=title]',
   inputDescription: '[ng-model$=description]',
-  inputBody: '[ng-model$=body]',
+  inputBody: '[ng-mode=l$=body]',
   inputTags: '[ng-model$=tagField]'
 
 }
