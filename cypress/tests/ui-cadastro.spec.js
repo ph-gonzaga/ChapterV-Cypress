943f553fefac28076f5c231e765f87ee89cb2c08
Pedro Henrique Gonzaga <henrique.gonzaga@live.com>
2021-10-26 11:39:57 -0300

    adiciona projeto v

---
+++ cypress/tests/ui-cadastro.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+describe('Cadastro', () => {
+    it('Cadastro com sucesso', () => {
+
+        cy.intercept({
+            method: 'POST',
+            path: `/api/users`
+
+        }, {
+            statusCode: 200,
+            fixture: 'cadastro',
+        }).as('postUsers')
+
+        cy.visit('register')
+        cy.get('[placeholder="Username"]').type('ChapterV1215')
+        cy.get('[placeholder="Email"]').type('te@test.com')
+        cy.get('[placeholder="Password"]').type('123456')
+        cy.get('button.btn-primary').click()
+
+        cy.contains('Loading articles...').should('be.visible')
+    });
+    it('Usuario existente', () => {
+
+        cy.intercept({
+            method: 'POST',
+            path: `/api/users`
+
+        }, {
+            statusCode: 422,
+            fixture: 'cadastro-usuario-existente',
+        }).as('postUsers')
+
+        cy.visit('register')
+        cy.get('[placeholder="Username"]').type('ChapterV1215')
+        cy.get('[placeholder="Email"]').type('te@test.com')
+        cy.get('[placeholder="Password"]').type('123456')
+        cy.get('button.btn-primary').click()
+
+        cy.contains('username has already been taken').should('be.visible')
+        cy.contains('email has already been taken').should('be.visible')
+    });
+});
